360ec588d1cecb2d501c36a3c0ca9dbcee8b4f91
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-02 17:03:09 -0700

    program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+ *Main Entry of my program
+ *Program that print something with puts method
+ *
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
