2910d09f85280753acdf7a53feddb4fc18b13ad7
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-02 19:19:12 -0700

    print date

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ *Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
