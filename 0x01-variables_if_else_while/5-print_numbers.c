d9ebf58724178cc9e248b7d26be1e8d022c162a7
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-03 20:01:16 -0700

    print decimal digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ *Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+int n = 10;
+
+for (int i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+
+return (0);
+}
