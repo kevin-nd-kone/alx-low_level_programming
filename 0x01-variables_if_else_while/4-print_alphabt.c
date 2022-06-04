a77107d273e034a45a63924553ed864049b6a769
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-03 19:50:07 -0700

    alphabet without some letter

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ *Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+char c;
+char l;
+for (c = 'a'; c <= 'z'; ++c)
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+}
+printf("\n");
+return (0);
+}
