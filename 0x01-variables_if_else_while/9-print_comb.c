1d9ac329d23f24bf83d7769c6fe68b09d63ff71e
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-05 16:40:13 -0700

    print number following by comma and white space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 success. Error code otherwise
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n % 10 + '0');
+putchar(',');
+putchar(' ');
+putchar('\n');
+}
+return (0);
+}
