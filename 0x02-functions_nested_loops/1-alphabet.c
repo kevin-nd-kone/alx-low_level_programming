07dcd81e9a7b8a97a3442a78d63a2428608e88a5
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-08 13:15:09 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
 
 void print_alphabet(void)
 {
-char alphabet;
+char a;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (a = 'a'; a <= 'z'; a++)
 {
-_putchar(alphabet);
+_putchar(a);
 }
 
 _putchar('\n');
