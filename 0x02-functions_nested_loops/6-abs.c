460ef9bf8c76eab03ea3d043aae420d9d1cbe8b6
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-08 14:21:31 -0700

    print absolute value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ * _abs - value absolute
  *
- * @n: the int to print
+ * @n: The int to print
  * Return: Always 0.
-*/
-int print_sign(int n)
+ */
+
+int _abs(int n)
 {
-	if (n > 0)
+
+	if (n < 0)
 	{
-		_putchar ('+');
-		return (1);
+		return (n * (-1));
 	}
 
 	else if (n == 0)
 	{
-		_putchar ('0');
 		return (0);
 	}
 
 	else
 	{
-		_putchar ('-');
-		return (-1);
+		return (n);
 	}
 
 }
