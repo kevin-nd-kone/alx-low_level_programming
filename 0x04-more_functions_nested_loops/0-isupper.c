0fbea51c0578eaf37f915f696e7180372a5fcb1e
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-09 14:33:39 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+*@c int
+*
+*@return 1 when c isuppercase and 0 otherwise
+*/
+
+int _isupper(int c)
+{
+  if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
