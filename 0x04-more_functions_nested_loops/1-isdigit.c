1bba5030c0d6f2db3fddc8983361b3ccca40cee0
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-09 15:15:25 -0700

    check digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - function that verify @c is digit.
+ *
+ * @c: char
+ *
+ * Return: 1 char isupper 0 otherwise.
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
