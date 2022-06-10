6c0de39bd760f39441993e3d6d9cd52fba53c640
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-09 17:04:58 -0700

    print more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print number for 0 - 9.
+ *
+ *
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+int i;
+int j = 0;
+while (j <= 10)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+}
+}
+j++;
+_putchar('\n');
+}
