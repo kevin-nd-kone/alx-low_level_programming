db55eb78b3e59d622bb0fbbd3c98561f359cc69f
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-13 15:07:54 -0700

    integer swrapper

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - interger swapper
+ *
+ *@a : param int
+ *@b : param int
+ *
+ *return - alway 0
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
