972be7e37396c422eb19d347237cdf05ae658a4b
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-13 16:05:43 -0700

    string len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - calculate len of string
+ *@n : param string
+ *Return int - int the len of string n
+ */
+int _strlen(char *n)
+{
+return (strlen(n));
+}
