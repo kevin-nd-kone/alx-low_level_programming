429f0cef6e8cc2a4f93913e8d28c44bb1a937bbb
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-13 14:54:23 -0700

    reset int value by he's pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset int value to 98 
+ *
+ * Return - alway 0
+ */
+
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+** My C array and string project **
