8ebfa813fc25491a445f218f568c0e5f9b31a5ba
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-13 17:46:54 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_rev - print rev string
+ *@s : param srting
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+puts(strrev(s)); 
+}
