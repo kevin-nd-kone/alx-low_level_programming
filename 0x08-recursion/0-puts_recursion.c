a0a1fd97f5d850613550f56998a4d9280c62a222
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-22 15:47:06 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts_recursion - print a string
+*
+*@s - char param
+*
+*/
+
+_puts_recursion(char *s)
+{
+int i = sizeof s / sizeof s[i];
+for(i = 0; i < i; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
