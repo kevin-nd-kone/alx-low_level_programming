5b936e7c59db1d990a1a81dfbc70af4c84ca215c
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-06-24 16:59:59 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main entry of program
+ *
+ *@argc : number of argument
+ *
+ *@argv : array of argument
+ *
+ *Return: alway 1 if erro otherwire 0
+ */
+
+int main(int argc, char *argv[])
+{
+if(!(argc > 2))
+{
+char error[] = "Error";
+printf("%s\n",error);
+return (1);
+}
+int i,res;
+res = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", res);
+return (0);
+}
