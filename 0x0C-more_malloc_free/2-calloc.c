e744328a85a0bbea24b037ddbd8235c91274a351
kevin <kevinkone19@gmail.com>
2022-07-01 01:12:45 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 
 /**
+ * *_calloc - allocates memory for an array, using malloc
+ * @memo: array length
+ * @size: size of each element
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int memo, unsigned int size)
+{
+void *m;
+
+if (size == 0 || memo == 0)
+return (NULL);
+
+m = malloc(memo * size);
+
+if (m == 0)
+return (NULL);
+
+_memoryset(m, 0, memo * size);
+
+return (m);
+}
+
+/**
  * *_memoryset - fills memory with a constant byte.
  * @cons: pointer to put the constant
  * @c: constant
@@ -17,28 +41,4 @@ char *_memoryset(char *cons, char c, unsigned int n)
         *cons++ = c;
 
     return (ptr);
-}
-
-/**
- * *_calloc - allocates memory for an array, using malloc
- * @memo: array length
- * @size: size of each element
- * Return: pointer
- */
-
-void *_calloc(unsigned int memo, unsigned int size)
-{
-    void *m;
-
-    if (size == 0 || memo == 0)
-        return (NULL);
-
-    m = malloc(memo * size);
-
-    if (m == 0)
-        return (NULL);
-
-    _memset(m, 0, memo * size);
-
-    return (m);
 }
