0b426ac3d0fdb585b42ef1b9d201dd3424c2e3b2
kevin <kevinkone19@gmail.com>
2022-07-04 20:31:44 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+**My C struct project readme file** 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * struct dog - define a struct dog
+ * 
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: a dog struct to store dog properties
+ */
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+};
