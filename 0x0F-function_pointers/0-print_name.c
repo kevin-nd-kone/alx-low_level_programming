0b512326789dbff91c8cb5154179c78d575d3c8e
kevin <kevinkone19@gmail.com>
2022-07-06 23:54:39 +0000

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,22 +4,22 @@
 /**
  * print_name - function that print name
  *
- * @name: param char 
+ * @name: param char
  * @f: param function pointer
- * 
+ *
  */
 
 void print(char *name);
 void print_name(char *name, void (*f)(char *))
 {
-if(f)
+if (f)
 f = &print;
-f(name);
+f (name);
 }
 
 
 void print(char *name)
 {
-if(name)
+if (name)
 printf("Hello, my name is %s", name);
 }
