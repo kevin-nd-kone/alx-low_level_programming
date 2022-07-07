4a5c6a5e44e596925bbb09a582e5d70f922946b6
kevin <kevinkone19@gmail.com>
2022-07-07 21:49:08 +0000

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdarg>
+/**
+ * sum_them_all - function that calculate the sum of all given params
+ *
+ * @n: param unsigned integer
+ * @...: other param
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int res = 0;
+std::va_lits args;
+va_start(args, count);
+for (int i = 0; i < count; ++i)
+{
+res += va_arg(args, int);
+}
+va_end(args);
+return result;
+}
