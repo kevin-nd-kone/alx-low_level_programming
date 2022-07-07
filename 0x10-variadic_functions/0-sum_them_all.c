799c2a3dda4bd5ae1df3f367b16789a772bd73c8
kevin <kevinkone19@gmail.com>
2022-07-07 22:27:41 +0000

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,22 +4,21 @@
  * sum_them_all - function that calculate the sum of all given params
  *
  * @n: param unsigned integer
- * @...: other param
  * Return: int
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int sum = 0;
+unsigned int res = 0;
 unsigned int i;
-va_list arguments;
+va_list arg;
 
-va_start(arguments, n);
+va_start(arg, n);
 
 if (n == 0)
 return (0);
 for (i = 0; i < n; i++)
 {
-sum += va_arg(arguments, unsigned int);
+res += va_arg(arg, unsigned int);
 }
-return (sum);
+return (res);
 }
