5a98422586ba27203d53cf4a139f70eb2651cb6a
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-07-15 00:48:55 +0000

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,29 +1,41 @@
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * *add_node - adds a new node at the beginning of a list_t list.
- * @head: pointer to first elements.
+ * *add_node_end - adds a new node at the end of a linked list.
+ * @head: start of the list.
  * @str: string.
- * Return: the address of the new elements,
- * or NULL if it failed.
+ * Return: the address of the new element,
+ * or null if it failed.
  */
-
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *newNode;
-
 newNode = (list_t *)malloc(sizeof(list_t));
 if (newNode == NULL)
 return (NULL);
 newNode->str = strdup(str);
 newNode->len = string_length(str);
-newNode->next = *head;
+newNode->next = NULL;
+
+if (*head == NULL)
 *head = newNode;
 
-return (*head);
+else
+{
+list_t *lastNode = *head;
+
+while (lastNode->next != NULL)
+{
+lastNode = lastNode->next;
 }
+
+lastNode->next = newNode;
+}
+return (newNode);
+}
+
 /**
  * string_length - finds the length of a string.
  * Return: length of c.
