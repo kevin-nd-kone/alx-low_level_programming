6dd30beccbe9acb9b6fcd6284a039613b78e5a0c
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-07-19 00:53:13 +0000

    10. Delete at index

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *reverse_listint - function that reversed a linked list.
+ *
+ * @head: pointer to head.
+ * Return: pointer to first node of reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *prev, *next;
+prev = NULL;
+while (*head != NULL)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = (*head);
+(*head) = next;
+}
+*head = prev;
+return (*head);
+}
