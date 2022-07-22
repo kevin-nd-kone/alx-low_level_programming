f755f7703b624dcf03c08b8d90a96be30b3f0019
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-07-22 01:10:44 +0000

    Create function that set value of bits to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,9 +3,9 @@
 /**
  * set_bit - function that sets the value of a bit to 1 at a given index.
  *
- * @n: unsigned long int param
- * @index: unsigned int param
- * Return: int
+ * @n: unsigned long int param.
+ * @index: unsigned int param.
+ * Return: integer 1 alway.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -13,7 +13,6 @@ if (index > 64)
 {
 return (-1);
 }
-
 *n ^= (1 << index);
 return (1);
 }
