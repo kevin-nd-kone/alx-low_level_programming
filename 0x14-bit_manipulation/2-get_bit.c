ad5f896d0ea894c48b89562f37ca827dfa976b39
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-07-22 00:39:56 +0000

    Create a function that return a bit of a given number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index.
+ *
+ * @n: unsigned long int param
+ * @index: unsigned int param
+ * Return: integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+const int num = 64;
+const int init = 0;
+if (index > num)
+return (-1);
+if (n == 0)
+return (0);
+n = n >> index;
+if ((n & 1) != 0)
+return (0);
+}
